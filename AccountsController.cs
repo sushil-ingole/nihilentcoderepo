d24da6536aad035b9c40bc0442ba93fc3a640ed3
Ravi Tambade <ravi.tambade@transflower.in>
2021-10-27 15:43:09 +0530

    new Controllers from MVC APP

---
+++ AccountsController.cs
@@ -0,0 +1,37 @@
+using System;
+//using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebMVCApp.Models;
+
+// This is new changes being added for filter addition
+namespace WebMVCApp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public Accounts(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+             
+           return View();
+        }
+
+        
+        
+        public IActionResult Aboutus(){
+               Console.WriteLine("Aboutus  Method is invoked.....");
+            
+             return View();
+        }
+
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,33 @@
+using System;
+//using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebMVCApp.Models;
+
+// This is new changes being added for filter addition
+namespace WebMVCApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ProductsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+             
+           return View();
+        } 
+        public IActionResult Products(){
+               Console.WriteLine("Products  Method is invoked.....");
+            
+             return View();
+        }
+    }
+}
